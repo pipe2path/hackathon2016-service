1b8af70ff299b1284f02e90caf0c192d729e3d93
Kevin <km0585@yp.com>
2016-08-30 15:26:34 -0700

    added initial files and code

---
+++ server.js
@@ -0,0 +1,12 @@
+// require the restify library.
+var restify = require('restify'),
+// create an HTTP server.
+server = restify.createServer();
+// add a route that listens on http://localhost:5000/hello/world
+server.get('/status', function (req, res, cb) {
+  res.send("It's occupied!");
+  return cb();
+});
+server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+  console.log('%s listening at %s', server.name, server.url);
+});
