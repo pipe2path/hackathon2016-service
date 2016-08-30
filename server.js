bd19e6d84c8f5fd4b3786c32ef91c83294659d91
Kevin <km0585@yp.com>
2016-08-30 15:46:20 -0700

    test1

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ server = restify.createServer();
 // add a route that listens on http://localhost:5000/hello/world
 server.get('/status', function (req, res, cb) {
   res.send("It's occupied!");
-  return cb();
+   return cb();
 });
-server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
-  console.log('%s listening at %s', server.name, server.url);
-});
+// server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+//   console.log('%s listening at %s', server.name, server.url);
+// });
