0303eccd7c580f192562a3c66014e3536c2ffeaa
Kevin <km0585@yp.com>
2016-08-30 15:56:03 -0700

    test2

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ server.get('/status', function (req, res, cb) {
   res.send("It's occupied!");
    return cb();
 });
-// server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
-//   console.log('%s listening at %s', server.name, server.url);
-// });
+server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+  console.log('%s listening at %s', server.name, server.url);
+});
