a9c1c2720e11ee85bbdc0dfc44124649b09ba49b
Kevin <km0585@yp.com>
2016-08-30 16:58:37 -0700

    changes

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 // require the restify library.
-var restify = require('restify'),
+var restify = require('restify');
+// sql server library
+var sql = require('mssql');
 // create an HTTP server.
 server = restify.createServer();
 // add a route that listens on http://localhost:5000/hello/world
 server.get('/status', function (req, res, cb) {
-  res.send("It's occupied!");
-   return cb();
+
+	var config = {
+		user: 'skatingclass',
+		password: 'Skating1!',
+		server: 'skatingclass.db.2259289.hostedresource.com',
+		database:'skatingclass'
+	};
+
+  	sql.connect(config, function(err){
+  		var query_str = "select c.Id, s.Name as Title, c.StartDateTime, c.EndDateTime, c.StudentId from Class c " +
+  					 "inner join Student s on c.StudentId = s.StudentId";
+  		var request = new sql.Request();
+
+		request.query(query_str, function(err, recordset){
+
+			if (err) console.log(err)
+
+			res.send(recordset);
+   		})
+  	})
 });
+
+
 server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
   console.log('%s listening at %s', server.name, server.url);
 });
