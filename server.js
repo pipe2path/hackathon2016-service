dd1c81bff34c74f7e86d74c6ab1e2a8a56743e21
Kevin <km0585@yp.com>
2016-08-31 13:49:57 -0700

    more changes2

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
 // require the restify library.
 var restify = require('restify');
 // sql server library
-var sql = require('mssql');
+var mysql = require('mysql');
 // create an HTTP server.
 server = restify.createServer();
 // add a route that listens on http://localhost:5000/hello/world
 server.get('/status', function (req, res, cb) {
 
-	var config = {
-		user: 'skatingclass',
-		password: 'Skating1!',
-		server: 'skatingclass.db.2259289.hostedresource.com',
-		database:'skatingclass'
-	};
+	//var config = {
+	//	user: 'skatingclass',
+	//	password: 'Skating1!',
+	//	server: 'skatingclass.db.2259289.hostedresource.com',
+	//	database:'skatingclass'
+	//};
+    //
+  	//sql.connect(config, function(err){
+  	//	var query_str = "select c.Id, s.Name as Title, c.StartDateTime, c.EndDateTime, c.StudentId from Class c " +
+  	//				 "inner join Student s on c.StudentId = s.StudentId";
+  	//	var request = new sql.Request();
+    //
+	//	request.query(query_str, function(err, recordset){
+    //
+	//		if (err) console.log(err)
+    //
+	//		res.send(recordset);
+   	//	})
+  	//})
 
-  	sql.connect(config, function(err){
-  		var query_str = "select c.Id, s.Name as Title, c.StartDateTime, c.EndDateTime, c.StudentId from Class c " +
-  					 "inner join Student s on c.StudentId = s.StudentId";
-  		var request = new sql.Request();
+	var connection = mysql.createConnection({
+		host     : 'hackathon2016.db.2259289.hostedresource.com',
+		user     : 'hackathon2016',
+		password : 'SusDevKev1!',
+		database : 'hackathon2016'
+	});
 
-		request.query(query_str, function(err, recordset){
+	connection.connect();
+	var sql_query = "select * from BathroomStatus bs inner join Bathroom b on bs.bathroomId = b.bathroomId inner join Floor f " +
+				"on b.floorId = f.floorId inner join Gender g on g.genderId = b.genderId ";
+	connection.query(sql_query, functio/n(err, rows, fields) {
+		if (err) throw err;
 
-			if (err) console.log(err)
-
-			res.send(recordset);
-   		})
-  	})
+		res.send(rows);
+	});
 });
 
 
-server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+server.listen(process.env.PORT || 5001, function () { // bind server to port 5000.
   console.log('%s listening at %s', server.name, server.url);
 });
